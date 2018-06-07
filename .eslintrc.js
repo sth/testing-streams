c75fd0c852b1ec731e160c732fe5a29a274f18e9
Stephan Hohe <sth.dev@tejp.de>
2018-06-07 03:07:21 +0000

    Added tool configurations

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": "2017",
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            "warn",
+            { "argsIgnorePattern": ".*_unused" }
+        ]
+    }
+};
