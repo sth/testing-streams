677c5dfe7d0f312f94a0798407876da5cc1858f6
Stephan Hohe <sth.dev@tejp.de>
2021-06-18 12:28:23 +0200

    Improve checknetwork

--- checknetwork.sh
+++ checknetwork.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function run() {
-	echo "$@"
+	echo "\$ $@"
 	"$@"
 }
 
@@ -9,5 +9,5 @@ run ifconfig
 run dig registry.yarnpkg.com
 
 dig registry.yarnpkg.com | grep '^yarn' | while read A B C D IP; do
-	run ping -n -c 10 "$IP"
+	run ping -n -c 5 -q "$IP"
 done
